ad88768e3cae2db71313ea22e978d46ae89d21fd
fatma soliman <fatmacivil@hotmail.com>
2024-09-04 17:16:23 +0300

    fixxx

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -30,6 +30,26 @@ class UsersController {
 
     return res.status(201).json({ id: newUser.insertedId, email });
   }
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.findUser({ _id: dbClient.ObjectID(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
 }
 
 export default UsersController;
